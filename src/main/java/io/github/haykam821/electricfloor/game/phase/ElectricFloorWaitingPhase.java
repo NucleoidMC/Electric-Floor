db10ea22a3f8344fb0a2ff937e4c5203711ed387
gegy1000 <gegy1000@gmail.com>
2020-08-21 18:33:21 +0200

    Plasmid 0.3.4: waiting lobby

--- src/main/java/io/github/haykam821/electricfloor/game/phase/ElectricFloorWaitingPhase.java
+++ src/main/java/io/github/haykam821/electricfloor/game/phase/ElectricFloorWaitingPhase.java
@@ -9,14 +9,12 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameMode;
 import xyz.nucleoid.plasmid.game.GameOpenContext;
+import xyz.nucleoid.plasmid.game.GameWaitingLobby;
 import xyz.nucleoid.plasmid.game.GameWorld;
 import xyz.nucleoid.plasmid.game.StartResult;
-import xyz.nucleoid.plasmid.game.config.PlayerConfig;
-import xyz.nucleoid.plasmid.game.event.OfferPlayerListener;
 import xyz.nucleoid.plasmid.game.event.PlayerAddListener;
 import xyz.nucleoid.plasmid.game.event.PlayerDeathListener;
 import xyz.nucleoid.plasmid.game.event.RequestStartListener;
-import xyz.nucleoid.plasmid.game.player.JoinResult;
 import xyz.nucleoid.plasmid.world.bubble.BubbleWorldConfig;
 
 import java.util.concurrent.CompletableFuture;
@@ -42,35 +40,19 @@ public class ElectricFloorWaitingPhase {
 			return context.openWorld(worldConfig).thenApply(gameWorld -> {
 				ElectricFloorWaitingPhase phase = new ElectricFloorWaitingPhase(gameWorld, map, context.getConfig());
 
-				gameWorld.openGame(game -> {
+				return GameWaitingLobby.open(gameWorld, context.getConfig().getPlayerConfig(), game -> {
 					ElectricFloorActivePhase.setRules(game);
 
 					// Listeners
 					game.on(PlayerAddListener.EVENT, phase::addPlayer);
 					game.on(PlayerDeathListener.EVENT, phase::onPlayerDeath);
-					game.on(OfferPlayerListener.EVENT, phase::offerPlayer);
 					game.on(RequestStartListener.EVENT, phase::requestStart);
 				});
-
-				return gameWorld;
 			});
 		});
 	}
 
-	private boolean isFull() {
-		return this.gameWorld.getPlayerCount() >= this.config.getPlayerConfig().getMaxPlayers();
-	}
-
-	public JoinResult offerPlayer(ServerPlayerEntity player) {
-		return this.isFull() ? JoinResult.gameFull() : JoinResult.ok();
-	}
-
 	public StartResult requestStart() {
-		PlayerConfig playerConfig = this.config.getPlayerConfig();
-		if (this.gameWorld.getPlayerCount() < playerConfig.getMinPlayers()) {
-			return StartResult.NOT_ENOUGH_PLAYERS;
-		}
-
 		ElectricFloorActivePhase.open(this.gameWorld, this.map, this.config);
 		return StartResult.OK;
 	}
